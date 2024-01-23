be672dbf786ea7834c6883245a1cf11f85be4860
Fatislay <fatination@gmail.com>
2024-01-23 14:31:02 +0100

    die aufgaben laufe endlich

---
+++ lieblinge.R
@@ -0,0 +1,12 @@
+#Datenrahmen erstellen
+data <- data.frame(
+  ID = c(1,2,3,4,5,6,7,8,9,10,11,12,14),
+  Alter = c(20,23,30,18,19,24,18,19,21,25,19,28,35),
+  Lieblingsfarbe = c("blau","grün","rot","blau","gelb","rot","rot","rot","grün",
+                     "grün","blau","gelb","blau"),
+  Lieblingstier = c("Elefant","Kuh","Hund","Katze","Hund","Hund","Hund","Giraffe",
+                    "Bär","Zebra","Pferd","Hase","Katze")
+)
+
+#CSV-Datei speichern
+write.csv(data, "lieblinge.csv", row.names = FALSE)
